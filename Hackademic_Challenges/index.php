e412c5449957b196b2a466207b23c5d3a66da7f5
Rui Caridade <rmcbc@users.noreply.github.com>
2018-05-16 10:42:32 +0100

    Add files via upload
    Inicial upload

---
+++ Hackademic_Challenges/index.php
@@ -0,0 +1,98 @@
+<?php
+
+/** 
+ *    ----------------------------------------------------------------
+ *    OWASP Hackademic Challenges Project
+ *    ----------------------------------------------------------------
+ *    Copyright (C) 2010-2011 
+ *   	  Andreas Venieris [venieris@owasp.gr]
+ *   	  Anastasios Stasinopoulos [anast@owasp.gr]
+ *    ----------------------------------------------------------------
+ */
+ 
+?>
+ 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+
+<head>
+<title>OWASP Hackademic Challenges</title>
+<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+
+<body>
+
+<table style="width: 100%" align="center" class="style7">
+	<tr>
+		<td class="style9" style="width: 123px">&nbsp;</td>
+		<td class="style2">
+<img src='challogox.png' /></td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><a href="about.php">About 
+		OWASP Hackademic Challenges</a></td>
+		<td class="style8">
+		<h1 class="style6">OWASP Hackademic Challenges Project</h1>
+		</td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px">Challenge 001 </td>
+		<td class="style9"><p>Our agents (hackers) informed us that there reasonable suspicion that  the site of this <a href="ch001/" target="_blank">Logistics Company</a> is a blind for a human organs'  smuggling organisation.<br /> <br /> This organisation attracts its victims through advertisments for jobs  with very high salaries. They choose those ones who do not have many  relatives, they assasinate them and then sell their organs to very rich  clients, at very high prices.<br /> <br /> These employees are registered in the secret files of the company as "special clients"!<br /> <br /> One of our agents has been hired as by the particular company. Unfortunately, since 01/01/2007 he has gone missing.<br /> <br /> We know that our agent is alive, but we cannot contact him. Last time he  communicated with us, he mentioned that we could contact him at the  e-mail address the company has supplied him with, should there a problem  arise.<br /> <br /> The problem is that when we last talked to him, he had not a company  e-mail address yet, but he told us that his e-mail can be found through  the company's site.<br /> <br /> The only thing we remember is that he was hired on Friday the 13th!<br /> <br /> You have to find his e-mail address and send it to us by using the central communication panel of the company's site.<br /> <br /> Good luck!!!</p>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 002 </span></td>
+		<td class="style9"><p>Your Country needs your help for finding the password of an enemy site  that contains useful information, which if is not acquired on time,  peace in our area will be at stake.<br /> <br /></p>
+<p>You must therefore succeed in finding the password of this military <a href="ch002/index.php" target="_blank">SITE</a>.<br /> <br /> Good luck!</p></td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 003 </span></td>
+		<td class="style9"><p>XSS permits a malevolent user to inject his own code in vulnerable web  pages. According to the OWASP 2010 Top 10 Application Security Risks,  XSS attacks rank 2nd in the "most dangerous" list.<br /> <br /> Your objective is to make an alert box appear  <a href="ch003/index.php" target="_blank">HERE</a> bearing the message: "<strong>XSS!</strong>".</p>
+		</td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 004 </span></td>
+		<td class="style9"><p>A hacker informed us that <a href="ch004/index.php" target="_blank">this</a> site suffers from an XSS-like type of  vulnerability. Unfortunately, he lost the notes he had written regarding  how exactly did he exploit the aforementioned vulnerability.</p>
+<p><br /> Your objective is to make an alert box appear, bearing the message "<strong>XSS!</strong>". It should be noted, however, that this site has some protection against such attacks.</p>
+		</td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 005 </span></td>
+		<td class="style9"><p>You need to get access to the contents of this <a href="ch005/index.php" target="_blank">SITE</a>. In order to achieve  this, however, you must buy the "p0wnBrowser" web browser. Since it is  too expensive, you will have to "fool" the system in some way, so that  it let you read the site's contents.</p>
+		</td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 006 </span></td>
+		<td class="style9"><p>In this assignment you must prove your... knightly skills! Real knights  have not disappeared.</p>
+<p>They still exist, keeping their secrets well  hidden.<br /> Your mission is to infiltrate their <a href="ch006/index.php" target="_blank">SITE</a>. There is a small problem, however... We don't know the password!<br /> Perhaps you could find it?</p>
+<p><br /> Let's see!<br /> g00d luck dudes!</p>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 007 </span></td>
+		<td class="style9"><p>A good friend of mine studies at Acme University, in the <a href="ch007/index.php" target="_blank">Computer  Science and Telecomms Department</a>. Unfortunately, her grades are not that  good. You are now thinking "This is big news!"... Hmmm, maybe not. What  is big news, however, is this: The network administrator asked for  3,000 euros to change her marks into A's. This is obviously a case of  administrative authority abuse. Hence... a good chance for D-phase and  public exposure...</p>
+<p><br /> I need to get into the site as admin and upload an index.htm  file in the web-root directory, that will present all required evidence  for the University's latest "re-marking" practices!<br /> I only need you to find the admin password for me...<br /> <br /> Good Luck!</p>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 008 </span></td>
+		<td class="style9"><p>You have managed, after several tries, to install a backdoor shell (Locus7Shell) to <a href="ch008/" target="_blank">trytohack.gr<br /></a> <br /> The problem is that, in order to execute the majority of the commands  (on the machine running the backdoor) you must have super-user rights  (root).<br /> <br /> Your aim is to obtain root rights.</p>
+		</td>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 009 </span></td>
+		<td class="style9"><p>A friend of yours has set up a news blog at <a href="ch009/index.php" target="_blank">slagoff.com</a>.  However, he is kind of worried regarding the security of the news that  gets posted on the blog and has asked you to check how secure it is.<br /> <br /> Your objective is to determine whether any vulnerabilities exist that, if exploited, can grant access to the blog's server.<br /> <br /> Hint: A specially-tailored backdoor shell can be found at "<a href="http://www.really_nasty_hacker.com/shell.txt" target="_blank">http://www.really_nasty_hacker.com/shell.txt</a>".</p>
+	</tr>
+	<tr>
+		<td class="style2" style="width: 123px"><span >Challenge 010 </span></td>
+		<td class="style9"><p>Would you like to become an active hacker ?<br /> How about becoming a member of the world's largest hacker group:<br /> The n1nJ4.n4x0rZ.CreW!<br /> <br /> Before you can join though, you 'll have to prove yourself worthy by passing the test that can be found at: <a href="ch010/" target="_blank">http://n1nj4h4x0rzcr3w.com</a><br /> <br /> If you succeed in completing the challenge, you will get a serial  number, which you will use for obtaining the password that will enable  you to join the group.<br /> <br /> Your objective is to bypass the authentication mechanism, find the  serial number and be supplied with your own username and password from  the admin team of the site.</p>
+	</tr>
+	</table>
+<div class="style1">
+
+<br>
+
+<p class="owaspSupport">Supported by OWASP<br><br>
+<img src="Circle_owasp_logo_nowhitebackground.png"><br>
+</p>
+
+</body>
+</html>
+
